6efb12e5840bf0b3454713d9948048c7056829f9
chandanch <chandan.cec107@gmail.com>
2023-07-16 13:37:45 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UsersList from './components/users-list/UsersList';
 const App = () => {
 	return (
 		<div className="container mx-auto">
+			<h1>Favsome Albums</h1>
 			<UsersList />
 		</div>
 	);
