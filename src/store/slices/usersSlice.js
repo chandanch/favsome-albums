c19f9b3e477cf5c3e91432e24a43c28eea71dd6d
chandanch <chandan.cec107@gmail.com>
2023-07-20 10:08:56 +0530

    bind reducers for getUsers async thunk

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getUsers } from '../thunks/getUsers';
 
 const usersSlice = createSlice({
 	name: 'users',
@@ -6,6 +7,21 @@ const usersSlice = createSlice({
 		isLoading: false,
 		data: [],
 		error: null,
+	},
+	extraReducers(builder) {
+		builder.addCase(getUsers.pending, (state, action) => {
+			state.isLoading = true;
+		});
+
+		builder.addCase(getUsers.fulfilled, (state, action) => {
+			state.data = action.payload;
+			state.isLoading = false;
+		});
+
+		builder.addCase(getUsers.rejected, (state, action) => {
+			state.error = action.error;
+			state.isLoading = false;
+		});
 	},
 });
 
