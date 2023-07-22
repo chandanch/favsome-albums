acfb665b1960c47788e9447a0cb12583de1823ff
chandanch <chandan.cec107@gmail.com>
2023-07-22 22:36:12 +0530

    extract users data from state and bind to comp :bulb:

--- src/components/users-list/UsersList.jsx
+++ src/components/users-list/UsersList.jsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../../store';
 
 const UsersList = () => {
 	const dispatch = useDispatch();
+	const { isLoading, data, error } = useSelector((state) => {
+		return state.users;
+	});
 
 	useEffect(() => {
 		dispatch(getUsers());
 	}, [dispatch]);
 
-	return <div>UsersList</div>;
+	if (isLoading) {
+		return <div>Fetching Users...</div>;
+	}
+
+	if (error) {
+		return <div>Error fetching users.</div>;
+	}
+
+	return <div> {data.length} </div>;
 };
 
 export default UsersList;
