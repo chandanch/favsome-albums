33f84cea1eb473d87fe278727c49dabbab6abaf1
chandanch <chandan.cec107@gmail.com>
2023-07-23 06:43:41 +0530

    add delay in get users action for testing

--- src/store/thunks/getUsers.js
+++ src/store/thunks/getUsers.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const getUsers = createAsyncThunk('users/get', async () => {
 	const response = await axios.get('http://localhost:3005/users');
+
+	await pause(3000);
+
 	return response.data;
 });
+
+const pause = (duration) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, duration);
+	});
+};
 
 export { getUsers };
