d525f612a07bb65f765d4e0f8a7b104e2d92e572
chandanch <chandan.cec107@gmail.com>
2023-08-03 20:38:47 +0530

    show sync spinner on add user click

--- src/components/users-list/UsersList.jsx
+++ src/components/users-list/UsersList.jsx
@@ -49,11 +49,10 @@ const UsersList = () => {
 		<div>
 			<div className="flex flex-row justify-between m-3">
 				<h1 className="m-2 text-xl">Users</h1>
-				{isAddingUser ? (
-					'Creating User'
-				) : (
-					<Button onClick={addNewUser}>Add New User</Button>
-				)}
+				<Button onClick={addNewUser} isLoading={isAddingUser}>
+					Add New User
+				</Button>
+
 				{addingUserError && 'Error Creating User'}
 			</div>
 			{renderUsersList()}{' '}
