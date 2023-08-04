d9473ae60e526e6579051f7dca4565d809cf0c41
chandanch <chandan.cec107@gmail.com>
2023-08-04 10:55:57 +0530

    add delay to delete for testing

--- src/store/thunks/deleteUser.js
+++ src/store/thunks/deleteUser.js
@@ -1,7 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { delay } from '../../utils/delay';
 
 export const deleteUser = createAsyncThunk('users/delete', async (id) => {
 	await axios.delete(`http://localhost:3005/users/${id}`);
+	await delay(3000);
 	return id;
 });
