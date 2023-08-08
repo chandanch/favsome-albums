5b0152e25cf90fb9a5c9207f7da093cbb8824775
chandanch <chandan.cec107@gmail.com>
2023-08-08 10:15:16 +0530

    use of create api hook to get albums data

--- src/components/albums-list/AlbumsList.jsx
+++ src/components/albums-list/AlbumsList.jsx
@@ -1,4 +1,10 @@
+import { useGetAlbumsQuery } from '../../store';
+
 const AlbumsList = ({ user }) => {
+	const { data, error, isLoading } = useGetAlbumsQuery(user);
+
+	console.log(data, error, isLoading);
+
 	return <div> Albums of {user.name} </div>;
 };
 
