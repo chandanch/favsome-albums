4969b7aa9c8669c50b7c44aa6b2b7015c1363a09
chandanch <chandan.cec107@gmail.com>
2023-08-10 09:09:54 +0530

    show albums list

--- src/components/albums-list/AlbumsList.jsx
+++ src/components/albums-list/AlbumsList.jsx
@@ -1,14 +1,37 @@
 import { useGetAlbumsQuery } from '../../store';
+import ExpandablePanel from '../expandable-panel/ExpandablePanel';
+import SkeltonLoader from '../loaders/skeleton-loader/SkeletonLoader';
 
 const AlbumsList = ({ user }) => {
-	const { data, error, isLoading, isFetching } = useGetAlbumsQuery(user);
+	const { data, error, isLoading } = useGetAlbumsQuery(user);
 
-	console.log(data, error, isLoading, isFetching);
+	let componentContent;
+	if (isLoading) {
+		componentContent = (
+			<SkeltonLoader skeletons={3} className="h-10 w-full" />
+		);
+	} else if (error) {
+		componentContent = <div>Failed to fetch Albums</div>;
+	} else {
+		componentContent =
+			data.length === 0 ? (
+				<div>No Albums</div>
+			) : (
+				(componentContent = data.map((album) => {
+					const header = <div>{album.name}</div>;
+					return (
+						<ExpandablePanel header={header} key={album.id}>
+							List of Photos!
+						</ExpandablePanel>
+					);
+				}))
+			);
+	}
 
 	return (
 		<div>
-			{' '}
-			Albums of {user.name} {data.length === 0 ? 'No Albums' : ''}{' '}
+			<h2>Albums of {user.name}</h2>
+			<div>{componentContent}</div>
 		</div>
 	);
 };
