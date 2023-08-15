1165d78579a7821a590060e53d4b647e6dcd34dc
chandanch <chandan.cec107@gmail.com>
2023-08-15 12:31:58 +0530

    bind photos api to store

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,28 @@ import { getUsers } from './thunks/getUsers';
 import { addUser } from './thunks/addUser';
 import { deleteUser } from './thunks/deleteUser';
 import { albumsApi } from './apis/albumsApi';
-import { useGetAlbumsQuery, useAddAlbumMutation, useDeleteAlbumMutation } from './apis/albumsApi';
+import {
+	useGetAlbumsQuery,
+	useAddAlbumMutation,
+	useDeleteAlbumMutation,
+} from './apis/albumsApi';
+import { photosAPi } from './apis/photosAPi';
+import {
+	useAddPhotoMutation,
+	useDeletePhotoMutation,
+	useGetPhotosQuery,
+} from './apis/photosAPi';
 
 const store = configureStore({
 	reducer: {
 		users: usersReducers,
 		[albumsApi.reducerPath]: albumsApi.reducer,
+		[photosAPi.reducerPath]: photosAPi.reducer,
 	},
 	middleware: (getDefaultMiddleware) => {
-		return getDefaultMiddleware().concat(albumsApi.middleware);
+		return getDefaultMiddleware()
+			.concat(albumsApi.middleware)
+			.concat(photosAPi.middleware);
 	},
 });
 
@@ -27,5 +40,8 @@ export {
 	deleteUser,
 	useGetAlbumsQuery,
 	useAddAlbumMutation,
-	useDeleteAlbumMutation
+	useDeleteAlbumMutation,
+	useAddPhotoMutation,
+	useGetPhotosQuery,
+	useDeletePhotoMutation,
 };
